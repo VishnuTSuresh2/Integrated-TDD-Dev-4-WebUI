d38f5904c1d586763173c59c3591ae1520cf86bf
Vishnu T Suresh <vishnutsuresh@live.com>
2017-04-08 00:37:27 +0530

    Navbar

--- dev.js
+++ dev.js
@@ -6,7 +6,22 @@ var container=document.createElement("div");
 container.id="mocha";
 document.body.appendChild(container);
 
-mocha.setup('bdd')
+mocha.setup({
+    ui:'bdd'
+})
+function mock(glb,callback){
+    let _glb=window[glb];
+    window[glb]=(...args)=>{
+        callback.apply(this,args);
+        _glb.apply(this,args);
+    }
+}
+mock("describe",(description,execute)=>{
+    
+});
+mock("it",(description,execute)=>{
+    
+});
 run(true); 
 
 mocha.run();
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar";
+import Navbar from "./Navbar/Navbar";
 import * as ReactDOM from "react-dom";
 import * as React from 'react';
 
